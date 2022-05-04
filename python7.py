ce6c1bea68fb3c16d3bcda460500dd3143700454
kucerova.anicka@gmail.com <kucerova.anicka@gmail.com>
2022-05-04 12:27:37 +0100

    8my ukol

--- python7.py
+++ python7.py
@@ -2,11 +2,10 @@ with open("posta.html", encoding='utf-8') as vstup:
     kodStranky = vstup.read()
 kodStranky.replace("\n", " ")
 import re
-vsechnyMezery = re.compile(r" +")
-nahrazeneMezery = vsechnyMezery.sub(" ", kodStranky)
+nahrazeneMezery = re.sub(" +"," ", kodStranky)
 
-pscMesto = re.compile(r"\d{3} ?\d{2} \w+ ?\d*")
+pscMesto = re.compile(r"\d{3} ?\d{2} [\w ]* ?\d*")
+print(nahrazeneMezery)
 vsechnaPSC = pscMesto.findall(nahrazeneMezery)
 for psc in vsechnaPSC:
   print(psc)
-
---
+++ python8.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+def vypocetVstupenek(kolikLidi,kdy):
+  # Datum převedu na python datum
+  kdy=datetime.strptime(kdy,"%d.%m.%Y")
+  #vytvořím hranice
+  start=datetime(2021,7,1)
+  stop=datetime(2021,8,31)
+  levnejsiVstupenky = datetime(2021,8,11)
+  if start <= kdy <= stop:
+    valid = True
+  else: 
+    valid = False
+  
+  if valid and (kdy >= levnejsiVstupenky):
+    cenaVstupenky = 180
+  elif valid and (kdy < levnejsiVstupenky):
+    cenaVstupenky = 250
+  else:
+    print("V tomhle termínu nehrajem, sorry.")
+  cena = int(kolikLidi) * cenaVstupenky
+  return f"Zaplať {cena} Kč."
+
+kolikLidi=input("Pro kolik osob chcete koupit vstupenku? ")
+kdy = input("Na který den chcete vstupenky? ")
+
+cena = vypocetVstupenek(kolikLidi, kdy)
+print(cena)
